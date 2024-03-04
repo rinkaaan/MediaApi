9888bf1f6ab75ffba3b4541dac285488610243fb
Lincoln Nguyen <137611486+rinkaaan@users.noreply.github.com>
2024-03-03 19:24:24 -0800

    Add commands.sh

---
+++ scripts/commands.sh
@@ -0,0 +1,3 @@
+scp -r root@hetzner:/root/workplace/Media/MediaApi/api/sqlite.db /Volumes/workplace/Media/MediaApi/api/sqlite.db
+
+scp -r /Volumes/workplace/Media/MediaApi/api/sqlite.db root@hetzner:/root/workplace/Media/MediaApi/api/sqlite.db
